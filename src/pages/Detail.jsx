46297f2b83103e13221a000dcfb952afb45f6839
user <oprenber@gmail.com>
2025-12-01 15:05:32 +0900

    POKEMON API

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,17 +1,23 @@
 import { useSelector } from "react-redux"
 import { useParams } from "react-router-dom"
 import { selectPokemonById } from "../RTK/selector"
+import FavoriteButton from "../component/FavoriteButton"
+import FlipCard from "../component/FilpCard"
 
 export default function Detail() {
     const {pokemonId} = useParams()
     const pokemon = useSelector(selectPokemonById(Number(pokemonId)))
     console.log(pokemon)
     return (
-    <div className="flex flex-col justify-center 
-    items-center border border-[gray] p-[30px] rounded-[10px]">
-        <div className="text-[28px] mb-[10px] ">{pokemon.name}</div>
+    <div className="bg-white flex flex-col justify-center 
+    items-center border py-[30px] px-[60px] rounded-[10px] border-b-[8px]
+    border-r-[8px] border-black">
+        <div className="text-[28px] mb-[10px] ">
+            {pokemon.name}
+            <FavoriteButton pokemonId={Number(pokemonId)} />
+        </div>
         <div className="whitespace-pre-wrap text-center">
         {pokemon.description}</div>
-        <img className="w-[200px]" src={pokemon.front}/>
+        <FlipCard front={pokemon.front} back={pokemon.back}/>
     </div>)
 }
